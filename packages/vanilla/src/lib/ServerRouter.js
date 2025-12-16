a07833dd37484f9ff62bfdc797124745fc173781
chacha <86880916+hansejun@users.noreply.github.com>
2025-12-16 21:50:50 +0900

    feat: implement ServerRouter for SSR

---
+++ packages/vanilla/src/lib/ServerRouter.js
@@ -0,0 +1,83 @@
+/**
+ * 서버 사이드 라우터
+ * window 의존성 없이 순수하게 URL 매칭만 수행
+ */
+export class ServerRouter {
+  #routes;
+  #baseUrl;
+
+  constructor(baseUrl = "") {
+    this.#routes = new Map();
+    this.#baseUrl = baseUrl.replace(/\/$/, "");
+  }
+
+  get baseUrl() {
+    return this.#baseUrl;
+  }
+
+  /**
+   * 라우트 등록
+   * @param {string} path - 경로 패턴 (예: "/product/:id")
+   * @param {Function} handler - 라우트 핸들러
+   */
+  addRoute(path, handler) {
+    // 경로 패턴을 정규식으로 변환
+    const paramNames = [];
+    const regexPath = path
+      .replace(/:\w+/g, (match) => {
+        paramNames.push(match.slice(1)); // ':id' -> 'id'
+        return "([^/]+)";
+      })
+      .replace(/\//g, "\\/");
+
+    const regex = new RegExp(`^${this.#baseUrl}${regexPath}$`);
+
+    this.#routes.set(path, {
+      regex,
+      paramNames,
+      handler,
+    });
+  }
+
+  /**
+   * URL 매칭 (서버용)
+   * @param {string} url - 매칭할 URL
+   * @returns {Object|null} 매칭된 라우트 정보
+   */
+  match(url) {
+    // URL 객체 생성 (origin은 임의로 설정, pathname만 필요)
+    const pathname = new URL(url, "http://localhost").pathname;
+
+    for (const [routePath, route] of this.#routes) {
+      const match = pathname.match(route.regex);
+      if (match) {
+        // 매치된 파라미터들을 객체로 변환
+        const params = {};
+        route.paramNames.forEach((name, index) => {
+          params[name] = match[index + 1];
+        });
+
+        return {
+          ...route,
+          params,
+          path: routePath,
+        };
+      }
+    }
+    return null;
+  }
+
+  /**
+   * 쿼리 파라미터를 객체로 파싱
+   * @param {string} search - 쿼리 문자열
+   * @returns {Object} 파싱된 쿼리 객체
+   */
+  static parseQuery(search = "") {
+    const params = new URLSearchParams(search);
+    const query = {};
+    for (const [key, value] of params) {
+      query[key] = value;
+    }
+    return query;
+  }
+}
