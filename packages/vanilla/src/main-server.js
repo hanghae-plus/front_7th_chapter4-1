36d5a049f81cae08991818a611c72e0dade10f38
sonsonsh1125 <tmdgus8911@gmail.com>
2025-12-18 00:09:45 +0900

    feat: Router 구현

--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,5 +1,10 @@
 import { HomePage, NotFoundPage, ProductDetailPage } from "./pages";
 import { BASE_URL } from "./constants.js";
+import { router } from "./router";
+
+// 서버 환경에서 라우트 등록
+router.addRoute("/", HomePage);
+router.addRoute("/product/:id", ProductDetailPage);
 
 // 서버 사이드 라우트 매칭 함수
 function findRoute(url, baseUrl = "") {
@@ -50,7 +55,7 @@ function findRoute(url, baseUrl = "") {
  * @param {Object} query - 쿼리 파라미터 객체 (나중에 라우터 구성 시 사용 예정)
  * @returns {Promise<string>} 렌더링된 HTML 문자열
  */
-// eslint-disable-next-line no-unused-vars
+
 export const render = async (url, query = {}) => {
   try {
     // BASE_URL 제거
@@ -58,6 +63,10 @@ export const render = async (url, query = {}) => {
 
     // 라우트 찾기
     const route = findRoute(cleanUrl, BASE_URL);
+
+    // 서버 환경에서 router 객체 설정
+    // router.setServerRoute를 사용하여 query와 params 설정
+    router.setServerRoute(cleanUrl, query, route.params);
 
     // 페이지 컴포넌트 실행
     const html = route.handler();
