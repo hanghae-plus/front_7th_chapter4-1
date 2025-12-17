c67cbb2dfef0ff1b8181834fcd80fa1e789673d9
hanseul jung <hanseul56524@gmail.com>
2025-12-18 02:55:47 +0900

    feat: hydration 추가

--- packages/vanilla/src/main.js
+++ packages/vanilla/src/main.js
@@ -4,6 +4,7 @@ import { registerAllEvents } from "./events.js";
 import { loadCartFromStorage } from "./services/index.js";
 import { router } from "./router/index.js";
 import { BASE_URL } from "./constants.js";
+import { productStore } from "./stores/productStore.js";
 
 const enableMocking = () =>
   import("./mocks/browser.js").then(({ worker }) =>
@@ -16,6 +17,18 @@ const enableMocking = () =>
   );
 
 function main() {
+  // hydration 처리
+  if (window.__INITIAL_DATA__) {
+    const { productState } = window.__INITIAL_DATA__;
+
+    // 상품 store 복원 (cartStore는 localStorage에서 로드해줌)
+    if (productState) {
+      productStore.dispatch({
+        type: "PRODUCT_ACTIONS.SETUP",
+        payload: productState,
+      });
+    }
+  }
   registerAllEvents();
   registerGlobalEvents();
   loadCartFromStorage();
