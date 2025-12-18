ae8dd48516edbebaa92f907b2da2505b44e0e3bb
Yongtae Park <samuel.park@mz.co.kr>
2025-12-19 01:45:34 +0900

    feat(vanilla): implement SSR server with Vite

--- packages/vanilla/server.js
+++ packages/vanilla/server.js
@@ -1,31 +1,46 @@
+import fs from "node:fs/promises";
 import express from "express";
+import { render } from "./src/main-server.js";
 
 const prod = process.env.NODE_ENV === "production";
 const port = process.env.PORT || 5173;
 const base = process.env.BASE || (prod ? "/front_7th_chapter4-1/vanilla/" : "/");
 
+// Cached production assets
+const templateHtml = prod ? await fs.readFile("./dist/client/index.html", "utf-8") : "";
+
 const app = express();
 
-const render = () => {
-  return `<div>안녕하세요</div>`;
-};
+// Add Vite or respective production middlewares
+/** @type {import('vite').ViteDevServer | undefined} */
+let vite;
+if (!prod) {
+  const { createServer } = await import("vite");
+  vite = await createServer({
+    server: { middlewareMode: true },
+    appType: "custom",
+    base,
+  });
+  app.use(vite.middlewares);
+} else {
+  const compression = (await import("compression")).default;
+  const sirv = (await import("sirv")).default;
+  app.use(compression());
+  app.use(base, sirv("./dist/client", { extensions: [] }));
+}
 
-app.get("*all", (req, res) => {
-  res.send(
-    `
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Vanilla Javascript SSR</title>
-</head>
-<body>
-<div id="app">${render()}</div>
-</body>
-</html>
-  `.trim(),
-  );
+// SSR Render
+app.get("*", async (req, res) => {
+  const url = req.originalUrl.replace(base, "");
+  const { html, head, initialDataScript } = await render(url);
+
+  // Template 치환
+  const finalHtml = templateHtml
+    .replace("<!--app-head-->", head)
+    .replace("<!--app-html-->", html)
+    .replace("</head>", `${initialDataScript}</head>`);
+
+  res.send(finalHtml);
 });
 
 // Start http server
--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,4 +1,14 @@
 export const render = async (url, query) => {
   console.log({ url, query });
-  return "";
+
+  // 1. Store 초기화
+  // 2. 라우트 매칭
+  // 3. 데이터 프리페칭
+  // 4. HTML 생성
+
+  return {
+    html: "<div>안녕하세요</div>",
+    head: "<title>Vanilla Javascript SSR</title>",
+    initialDataScript: "<script>console.log('initialData')</script>",
+  };
 };
