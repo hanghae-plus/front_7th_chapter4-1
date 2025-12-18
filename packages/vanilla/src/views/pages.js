0f657e69625747fbaa6e410c51e4078c34b3977c
이정민 <ljm5138@douzone.com>
2025-12-18 21:18:49 +0900

    refactor: 컴포넌트 공통화

---
+++ packages/vanilla/src/views/pages.js
@@ -0,0 +1,3 @@
+export { renderHomePageView } from "./homeView.js";
+export { renderProductDetailPageView } from "./productDetailView.js";
+export { renderNotFoundPageView } from "./notFoundView.js";
