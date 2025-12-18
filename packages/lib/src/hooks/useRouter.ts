20a1443d0185ee4aec3bf0d226ab9cb994732a04
rlacodud <rlacodud0313@naver.com>
2025-12-19 00:05:27 +0900

    feat(lib): React hooks에 SSR 호환성 추가

--- packages/lib/src/hooks/useRouter.ts
+++ packages/lib/src/hooks/useRouter.ts
@@ -3,9 +3,33 @@ import type { AnyFunction } from "../types";
 import { useSyncExternalStore } from "react";
 import { useShallowSelector } from "./useShallowSelector";
 
+/**
+ * 기본 셀렉터 함수
+ * 상태를 그대로 반환하거나 타입 변환만 수행
+ */
 const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
+/**
+ * Router를 React 훅으로 사용하기 위한 커스텀 훅
+ *
+ * 라우터 상태 변경 시 컴포넌트를 자동으로 리렌더링함
+ * SSR 호환성을 위해 useSyncExternalStore 사용
+ *
+ * @template T - Router 인스턴스 타입
+ * @template S - 셀렉터가 반환하는 타입
+ *
+ * @param router - Router 인스턴스
+ * @param selector - 라우터 상태에서 필요한 부분만 선택하는 함수 (옵션)
+ *
+ * @returns 셀렉터가 반환한 값 (기본값은 router 인스턴스 전체)
+ */
 export const useRouter = <T extends RouterInstance<AnyFunction>, S>(router: T, selector = defaultSelector<T, S>) => {
+  // 얕은 비교를 통해 불필요한 리렌더링 방지
   const shallowSelector = useShallowSelector(selector);
-  return useSyncExternalStore(router.subscribe, () => shallowSelector(router));
+
+  // useSyncExternalStore를 사용하여 외부 스토어(라우터) 구독
+  return useSyncExternalStore(
+    router.subscribe, // 라우터 상태 변경 구독
+    () => shallowSelector(router), // 현재 라우터 상태 반환 (클라이언트/서버 동일)
+  );
 };
--- packages/lib/src/hooks/useStore.ts
+++ packages/lib/src/hooks/useStore.ts
@@ -2,11 +2,38 @@ import type { createStore } from "../createStore";
 import { useSyncExternalStore } from "react";
 import { useShallowSelector } from "./useShallowSelector";
 
+/**
+ * createStore가 반환하는 스토어 타입
+ */
 type Store<T> = ReturnType<typeof createStore<T>>;
 
+/**
+ * 기본 셀렉터 함수
+ * 상태를 그대로 반환하거나 타입 변환만 수행
+ */
 const defaultSelector = <T, S = T>(state: T) => state as unknown as S;
 
+/**
+ * Store를 React 훅으로 사용하기 위한 커스텀 훅
+ *
+ * 스토어 상태 변경 시 컴포넌트를 자동으로 리렌더링함
+ * SSR 호환성을 위해 useSyncExternalStore 사용
+ *
+ * @template T - 스토어 상태 타입
+ * @template S - 셀렉터가 반환하는 타입
+ *
+ * @param store - createStore로 생성한 스토어 인스턴스
+ * @param selector - 스토어 상태에서 필요한 부분만 선택하는 함수 (옵션)
+ *
+ * @returns 셀렉터가 반환한 값 (기본값은 스토어 상태 전체)
+ */
 export const useStore = <T, S = T>(store: Store<T>, selector: (state: T) => S = defaultSelector<T, S>) => {
+  // 얕은 비교를 통해 불필요한 리렌더링 방지
   const shallowSelector = useShallowSelector(selector);
-  return useSyncExternalStore(store.subscribe, () => shallowSelector(store.getState()));
+
+  // useSyncExternalStore를 사용하여 외부 스토어 구독
+  return useSyncExternalStore(
+    store.subscribe, // 스토어 상태 변경 구독
+    () => shallowSelector(store.getState()), // 현재 스토어 상태 반환 (클라이언트/서버 동일)
+  );
 };
