32a86154f268cafd7289da09e62b213eef2a9c5e
seunghoonKang <tmdgns4321@gmail.com>
2025-12-19 04:50:56 +0900

    feat: SSR 페이지 렌더링 구현

--- packages/vanilla/src/main-server.js
+++ packages/vanilla/src/main-server.js
@@ -1,4 +1,34 @@
+import { HomePage, NotFoundPage, ProductDetailPage } from "./pages";
+import { router } from "./router";
+import { PRODUCT_ACTIONS, productStore } from "./stores";
+
 export const render = async (url, query) => {
-  console.log({ url, query });
-  return "";
+  router.addRoute("/", HomePage);
+  router.addRoute("/product/:id/", ProductDetailPage);
+  router.addRoute(".*", NotFoundPage);
+  router.start();
+  router.push(url);
+  router.query = query;
+
+  const PageComponent = router.target;
+  const initData = await PageComponent.getServerSideProps?.();
+
+  if (PageComponent === HomePage) {
+    productStore.dispatch({
+      type: PRODUCT_ACTIONS.SETUP,
+      payload: {
+        products: initData.products,
+        categories: initData.categories,
+        totalCount: initData.pagination.total,
+        loading: false,
+        status: "done",
+      },
+    });
+  }
+
+  return {
+    head: `<title>${PageComponent.title || "쇼핑몰"}</title>`,
+    html: PageComponent(),
+    data: initData,
+  };
 };
