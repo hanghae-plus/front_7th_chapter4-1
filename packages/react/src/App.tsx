4fa5de91706c31682bb5bb0b1132d398bd10888b
dasom <ds92ko@gmail.com>
2025-12-19 16:41:27 +0900

    fix: React 상세페이지 CSR 이동 시 페이지 타이틀 업데이트 문제 수정
    - App 컴포넌트에서 currentProduct를 useEffect 의존성 배열에 추가
    - 상품 데이터 로드 후 타이틀이 '상품명 - 쇼핑몰'로 업데이트되도록 수정

--- packages/react/src/App.tsx
+++ packages/react/src/App.tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "@hanghae-plus/lib";
 import { useEffect } from "react";
 import { ModalProvider, ToastProvider } from "./components";
-import { useLoadCartStore } from "./entities";
+import { useLoadCartStore, useProductStore } from "./entities";
 import { useCurrentPage, useRouterContext } from "./router";
 import { updateTitle } from "./utils/updateTitle";
 
@@ -17,10 +17,11 @@ export const App = () => {
   const router = useRouterContext();
   const PageComponent = useCurrentPage();
   const route = useRouter(router, ({ route: r }) => r);
+  const { currentProduct } = useProductStore();
 
   useEffect(() => {
     updateTitle(router);
-  }, [router, route]);
+  }, [router, route, currentProduct]);
 
   return (
     <>
